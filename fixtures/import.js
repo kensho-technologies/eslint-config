2d81d00e04f97070707031dd256c43a724516bc6
Billy Janitsch <billy@kensho.com>
2019-07-05 15:50:13 -0400

    Consolidate JS and TS configs

--- fixtures/import.js
+++ fixtures/import.js
@@ -2,4 +2,4 @@ import something, {foo, baz} from './exports' // baz is not exported
 import somethingElse from './nonexistent' // file does not exist
 
 export {foo, baz, something, somethingElse}
-export {bar} from '../js/exports'
+export {bar} from './exports'
