99b912937693ef7ce1148baf15f6d224cb06f817
Billy Janitsch <billy@kensho.com>
2019-07-06 11:22:37 -0400

    Disable rules covered by the TS compiler

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ module.exports = {
 
     // these are checked by the TS compiler
     '@typescript-eslint/no-unused-vars': 0,
+    'import/default': 0,
     'import/named': 0,
+    'import/namespace': 0,
+    'import/no-unresolved': 0,
+    'react/no-unused-prop-types': 0,
+    'react/prop-types': 0,
   },
 }
