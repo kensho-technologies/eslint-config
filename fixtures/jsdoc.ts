a888b6bf80b81e697bf4418417e30007af628dc8
Billy Janitsch <billy@kensho.com>
2019-10-22 14:02:19 -0400

    Lint JSDoc comments (#44)
    * Lint JSDoc comments
    
    * Loosen requirement that params/return are documented
    
    * Add fixture

---
+++ fixtures/jsdoc.ts
@@ -0,0 +1,26 @@
+/**
+ * Adds two numbers.
+ *
+ * @param a The first number to add.
+ * @param b The second number to add.
+ * @returns The sum.
+ */
+function add(a: number, b: number): number {
+  return a + b
+}
+
+/**
+ * Subtracts two numbers.
+ */
+function subtract(a: number, b: number): number {
+  return a - b
+}
+
+function multiply(a: number, b: number): number {
+  return a * b
+}
+
+/* a regular, non-jsdoc comment */
+function divide(a: number, b: number): number {
+  return a / b
+}
