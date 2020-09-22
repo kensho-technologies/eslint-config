5392f4821a7dd5f8b98123044ba6e987e3ab626e
Billy Janitsch <billy@kensho.com>
2020-09-22 18:35:12 -0400

    Use TS version of no-shadow

--- index.js
+++ index.js
@@ -184,6 +184,10 @@ module.exports = {
         'no-use-before-define': 0,
         '@typescript-eslint/no-use-before-define': 2,
 
+        // exchange base rule for TS-specific rule
+        'no-shadow': 0,
+        '@typescript-eslint/no-shadow': 2,
+
         // allow ts-ignore comments
         '@typescript-eslint/ban-ts-comment': 0,
 
