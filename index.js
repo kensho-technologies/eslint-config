75858fe67bf4f04476731e29eb1540bfa2e42567
Billy Janitsch <billy@kensho.com>
2022-09-06 15:44:06 -0400

    Avoid checking for unknown React properties in TypeScript

--- index.js
+++ index.js
@@ -175,6 +175,7 @@ module.exports = {
         'react/no-unused-prop-types': 0,
         'react/prop-types': 0,
         'react/react-in-jsx-scope': 0,
+        'react/no-unknown-property': 0,
 
         'default-param-last': 0,
         '@typescript-eslint/default-param-last': 2,
