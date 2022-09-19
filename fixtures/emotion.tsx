6c4a57e2c3916383153564099964fa19ef8234a8
Billy Janitsch <billy@kensho.com>
2022-09-19 14:06:42 -0400

    Add ClassNames block to Emotion fixture

--- fixtures/emotion.tsx
+++ fixtures/emotion.tsx
@@ -1,4 +1,4 @@
-import {css} from '@emotion/react'
+import {ClassNames, css} from '@emotion/react'
 
 const divCss = css`
   background: blue;
@@ -10,10 +10,11 @@ interface MyComponentProps {
 }
 
 export default function MyComponent(props: MyComponentProps): JSX.Element {
-  const {a} = props
+  const {a, b} = props
   return (
     <div css={divCss}>
       <button type="button">{a}</button>
+      <ClassNames>{({css}) => <div className={css({background: 'red'})}>{b}</div>}</ClassNames>
     </div>
   )
 }
