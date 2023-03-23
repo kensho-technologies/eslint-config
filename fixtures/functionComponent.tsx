2a1f14024285e3c96f288d45204e7e28b32bec43
Billy Janitsch <billy@kensho.com>
2023-03-23 12:56:29 -0400

    Add unused rest sibling to fixture

--- fixtures/functionComponent.tsx
+++ fixtures/functionComponent.tsx
@@ -4,14 +4,15 @@ const ThemeContext = createContext({color: 'red'})
 
 interface ButtonProps {
   text: string
+  unused: number
 }
 
 export default function Button(props: ButtonProps): JSX.Element {
-  const {text} = props
+  const {text, unused, ...rest} = props
   const theme = useContext(ThemeContext)
   const [count, setCount] = useState(1)
   return (
-    <div style={{color: theme.color}}>
+    <div style={{color: theme.color}} {...rest}>
       <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
         {text}
       </button>
