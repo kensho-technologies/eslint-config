724e42877c98845077810e9c6ef5a02a51062a2b
Billy Janitsch <billy@kensho.com>
2024-11-14 15:10:25 -0500

    Add React compiler plugin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
 
 export default ts.config(
@@ -8,10 +9,12 @@ export default ts.config(
   ...ts.configs.recommended,
   {
     plugins: {
+      'react-compiler': reactCompiler,
       'react-hooks': reactHooks,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', {ignoreRestSiblings: true}],
+      'react-compiler/react-compiler': 'error',
       'react-hooks/exhaustive-deps': 'error',
       'react-hooks/rules-of-hooks': 'error',
     },
