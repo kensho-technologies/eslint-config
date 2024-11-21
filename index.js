6e159cdc8aaa69fe55614e74aac9df0ca3903a5c
Billy Janitsch <billy@kensho.com>
2024-11-21 17:25:13 -0500

    Add TanStack Query plugin

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import query from '@tanstack/eslint-plugin-query'
 import reactHooks from 'eslint-plugin-react-hooks'
 
 export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
+  ...query.configs['flat/recommended'],
   {
     plugins: {
       'react-hooks': reactHooks,
